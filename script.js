b5c48ca78f77f5eab87de2b15019285495c92f79
miuhuru <zermoun.yasmine@gmail.com>
2022-02-18 20:00:02 +0100

    figured out how to display localstorage data to the web page

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
+const container = document.createElement("div");
+container.id = "container";
+document.body.appendChild(container);
+
+const namy = document.createElement("input")
+namy.type = "text"
+namy.classList.add("name")
+
+const napy = document.createElement("input")
+napy.type = "text"
+napy.classList.add("rating")
+
+const button = document.createElement("button")
+button.id = "button"
+button.innerText = "Enter"
+
+container.appendChild(namy)
+container.appendChild(napy)
+container.appendChild(button)
+
 let all = []
-let over
+let parsed
 
-const toast = {
-  name: "toast",
-  rating: 7,
-};
-
-const panini = {
-    name: "panini",
-    rating: 8,
+let Make = class {
+    constructor(name, rating) {
+      this.name = name;
+      this.rating = rating;
+    }
 }
 
-all.push(toast)
-all.push(panini)
+makeThem = () => {
+    let item = new Make()
+    item.name = document.querySelector(".name").value 
+    item.rating = document.querySelector(".rating").value
+    all.push(item)
+}
 
 const displayData = (item) => {
-    const container = document.createElement("div");
-    container.id = "container";
-    document.body.appendChild(container);
+    const container = document.getElementById("container")
     const p = document.createElement("p");
     p.id = "name";
     p.innerText = "name: " + item.name;
@@ -35,15 +54,32 @@ const setlocalStorage = (item) => {
 }
 
 const parseIt = (item) => {
-    over = JSON.parse(localStorage.getItem(item))
+    parsed = JSON.parse(localStorage.getItem(item))
 }
 
-const displayFromArray = () => {
-    all.forEach(obj => {
+const displayFromArray = () => { // this function displays all objects everytime it is called, which is not ideal
+    array = all
+    array.forEach(obj => {
         setlocalStorage(obj)
         parseIt(obj)
         displayData(obj)
     })
 }
 
-displayFromArray()
+const displaying = () => {   // this function only displays the last item added
+    array = all
+    let last = array.slice(-1)[0]
+    last.name = array.slice(-1)[0].name 
+    last.rating = array.slice(-1)[0].rating 
+    setlocalStorage(last)
+    parseIt(last)
+    displayData(last)
+}
+
+button.addEventListener("click", () => {
+    makeThem()
+    console.table(all)
+    displaying()
+    document.querySelector(".name").value = "";
+    document.querySelector(".rating").value = "";
+})
