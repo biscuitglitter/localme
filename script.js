67c286dd12102fd9e99ec73395bd09e91b9a1c7f
miuhuru <zermoun.yasmine@gmail.com>
2022-02-19 13:58:46 +0100

    finally figured it out

--- script.js
+++ script.js
@@ -6,80 +6,68 @@ const namy = document.createElement("input")
 namy.type = "text"
 namy.classList.add("name")
 
-const napy = document.createElement("input")
-napy.type = "text"
-napy.classList.add("rating")
+const ccc = document.createElement("div")
+ccc.id = "items-collector"
+
+const contain = document.createElement("div")
+contain.id = "buttons"
 
 const button = document.createElement("button")
-button.id = "button"
-button.innerText = "Enter"
+button.id = "set-data"
+button.innerText = "Set"
+
+const otherbutton = document.createElement("button")
+otherbutton.id = "get-data"
+otherbutton.innerText = "Get"
 
 container.appendChild(namy)
-container.appendChild(napy)
-container.appendChild(button)
+container.appendChild(contain)
+contain.appendChild(button)
+contain.appendChild(otherbutton)
+container.appendChild(ccc)
 
-let all = []
-let parsed
+let item 
 
-let Make = class {
-    constructor(name, rating) {
+let Make = class {                   
+    constructor(name) {
       this.name = name;
-      this.rating = rating;
     }
 }
 
-makeThem = () => {
+if (localStorage.getItem(item) === null) {         
+    console.log("setting item to empty array! []")
+    localStorage.setItem(item, JSON.stringify([]))  // if localstorage is null, i set items to an empty array //
+}     
+
+let all = JSON.parse(localStorage.getItem(item))  // every item that is saved into localstorage will be parsed and saved as the array all //
+
+makeItem = () => {        
     let item = new Make()
     item.name = document.querySelector(".name").value 
-    item.rating = document.querySelector(".rating").value
-    all.push(item)
+    all.push(item)        // every item made will be pushed into the array all //
 }
 
-const displayData = (item) => {
-    const container = document.getElementById("container")
+const displayItems = (item) => {
+    let collector = document.getElementById("items-collector")
     const p = document.createElement("p");
     p.id = "name";
-    p.innerText = "name: " + item.name;
-  
-    const rating = document.createElement("p");
-    rating.id = "rating";
-    rating.innerText = "rating: " + item.rating;
-  
-    container.appendChild(p);
-    container.appendChild(rating);
-}
-  
-const setlocalStorage = (item) => {
-    localStorage.setItem(item, JSON.stringify(item));
-}
-
-const parseIt = (item) => {
-    parsed = JSON.parse(localStorage.getItem(item))
-}
-
-const displayFromArray = () => { // this function displays all objects everytime it is called, which is not ideal
-    array = all
-    array.forEach(obj => {
-        setlocalStorage(obj)
-        parseIt(obj)
-        displayData(obj)
-    })
-}
-
-const displaying = () => {   // this function only displays the last item added
-    array = all
-    let last = array.slice(-1)[0]
-    last.name = array.slice(-1)[0].name 
-    last.rating = array.slice(-1)[0].rating 
-    setlocalStorage(last)
-    parseIt(last)
-    displayData(last)
+    p.innerText = item.name;
+    collector.appendChild(p);
 }
 
 button.addEventListener("click", () => {
-    makeThem()
-    console.table(all)
-    displaying()
+    makeItem()
+    localStorage.setItem(item, JSON.stringify(all));   // i save the items in the array all //
     document.querySelector(".name").value = "";
-    document.querySelector(".rating").value = "";
 })
+
+otherbutton.addEventListener("click", () => {
+    let last = all.slice(-1)[0]
+    displayItems(last)      
+})
+
+
+all.forEach(item => {
+    displayItems(item)
+})
+
